23f984949643e50071a3ab879f2385abac385199
Jordan Hudgens <andthesunrises@gmail.com>
2017-06-12 15:07:19 +0000

    added seed for blog portfolio skills

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",  
+    body: "Some content some content #{blog}"
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",  
+    percentage_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio #{portfolio_item}",  
+    subtitle: "My great service",
+    body: "Some latin some latin",
+    main_image: "http://placehold.it/600x400", 
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+puts "9 portfolios created"
