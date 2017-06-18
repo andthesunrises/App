7f5cf1074888067cdcdc1c90bbe131ac29da545e
Jordan Hudgens <andthesunrises@gmail.com>
2017-06-18 05:30:05 +0000

    implemented image default

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,7 @@
 class Portfolio < ApplicationRecord
+  #inclue the little concern for placeholder image generator
+  include Placeholder
+  
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image  
   
   def self.angular
@@ -13,7 +16,7 @@ class Portfolio < ApplicationRecord
   # if self.main_image == nil
   # self.main_image = "whateveR"
   def set_defaults
-    self.main_image ||= "http://placehold.it/600x400"
-    self.thumb_image ||= "http://placehold.it/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
